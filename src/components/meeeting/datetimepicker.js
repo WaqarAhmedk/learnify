0ad4922e62a6cd5a88941d6e1364e0d5e644ba11
Muhammad Ali <106481105+m-optimistic-ali@users.noreply.github.com>
2022-09-13 20:34:39 +0500

    changes 9/13/22

---
+++ src/components/meeeting/datetimepicker.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Datetimepicker(props) {
+    return (
+      
+        
+        <section class="container">
+            <div className="input-group mb-3">
+            <label for="exampleFormControlTextarea1" className="form-label">{props.title}</label>
+            <form>
+                <div class="row form-group">
+                    <label for="date" class="col-sm-1 col-form-label">Date</label>
+                    <div class="col-sm-4">
+                        <div class="input-group date" id="datepicker">
+                            <input type="text" class="form-control" />
+                            <span class="input-group-append">
+                                <span class="input-group-text bg-white">
+                                    <i class="fa fa-calendar"></i>
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            </div>
+        </section>
+
+  
+  )
+}
