88b140340f54d96da6e928bdce429a63aebdd99f
Waqar Ahmed <wlwaqarah0@gmail.com>
2022-09-19 12:08:12 +0500

    added online clas est file

--- src/components/meeeting/callpage/callpage.js
+++ src/components/meeeting/callpage/callpage.js
@@ -1,19 +1,67 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import MeetingHeader from '../meetingheader/meetingheader';
 import MeetingFooter from '../meetingfooter/meetingfooter';
 import MeetingInfo from '../meetinginfo/meetinginfo';
 import MeetingMessages from '../meetingmessages/meetingmessages';
-import "./callpage.css"
+import "./callpage.css";
+import { useEffect } from 'react';
+
 
 export default function CallPage() {
+  const videoref = useRef(null);
+
+
+  const videoRef = useRef(null);
+
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 300 } })
+      .then(stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(err => {
+        console.error("error:", err);
+      });
+  };
+
+useEffect(()=>{
+  getVideo();
+},[videoRef])
+
+
+
+  const getvideo = () => {
+    navigator.mediaDevices.getUserMedia({
+      video: { width: 300 },
+      audio: true,
+
+    }).then((stream) => {
+      let video = videoref.current;
+      video.srcObject = stream;
+      video.play();
+    })
+  }
+
+
   return (
-    <div className='callpage-container'>
-      <video className='video-container' src='' controls>
-      </video>
-      <MeetingHeader />
-      <MeetingInfo />
-      <MeetingFooter />
-      <MeetingMessages />
+    <div>
+      <button>Take a photo</button>
+      <video ref={videoRef} />
+      <canvas />
     </div>
+
+    // <div className='callpage-container'>
+
+    //   {/* <video className='video-container' ref={videoref} >
+    //   </video> */}
+
+    //   {/* <MeetingHeader />
+    //   <MeetingInfo />
+    //   <MeetingFooter />
+    //   <MeetingMessages /> */}
+    // </div>
   )
 }
